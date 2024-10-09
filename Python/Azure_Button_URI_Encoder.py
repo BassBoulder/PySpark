2073ec1814d8502711e995c6d5786518a22c03cf
Nick Elgar <elgar89@icloud.com>
2024-10-09 07:57:24 +0100

    merged with Python Repo

---
+++ Python/Azure_Button_URI_Encoder.py
@@ -0,0 +1,29 @@
+#Import OS & Parse Functions
+from os import replace
+import urllib.parse
+
+# Leave Unchanged - This is the URI to the Microsoft Portal
+Prefix = 'https://portal.azure.com/#create/Microsoft.Template/uri/'
+
+# Change - This is the URI to your Storage Container
+ContainerRoot = '<Storage Container>'
+
+# Change - This is the name of your File within the Storage Container
+ContainerFile = '<fileanme.json>'
+
+# Change - This is the SAS Key to your Storage Container (NO NOT LEAVE PUBLICLY AVAILABLE)
+SAS = '<SAS KEY>'
+
+
+# ENCODING PREPERATION
+#################
+FileAndSAS = (f'{ContainerFile}?{SAS}')
+EncodedRoot = urllib.parse.quote(ContainerRoot)
+EncodedFileAndSAS = urllib.parse.quote(FileAndSAS)
+ReplacedEncodedRoot = EncodedRoot.replace("/", "%2F")
+#################
+# ENCODING OUTPUT
+EncodedURL = (f'{Prefix}{ReplacedEncodedRoot}{EncodedFileAndSAS}')
+
+# Copy - This is what you need to copy into your "Deploy to Azure" button wrapper code
+print(EncodedURL)
--- README.md
+++
@@ -1,3 +0,0 @@
-This Repo focuses directly on PySpark variant Python & SQL Code.
-
-I will add to this over time. 
