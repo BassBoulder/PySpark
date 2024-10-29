393a11eb0df8d60b355a055a28d16785adac7047
Nick <90762430+BassBoulder@users.noreply.github.com>
2024-10-29 15:44:28 +0000

    Added PDFMerge

---
+++ Python Variant/PDFMERGE/PDFMerge.py
@@ -0,0 +1,43 @@
+import PyPDF2
+
+ 
+
+def merge_pdfs(input_pdfs, output_pdf):
+
+    merger = PyPDF2.PdfMerger()
+
+ 
+
+    try:
+
+        for pdf in input_pdfs:
+
+            merger.append(pdf)
+
+ 
+
+        with open(output_pdf, 'wb') as merged_pdf:
+
+            merger.write(merged_pdf)
+
+        
+
+        print(f'Merged PDFs successfully. Output saved to: {output_pdf}')
+
+ 
+
+    except Exception as e:
+
+        print(f'Error merging PDFs: {e}')
+
+ 
+
+# Example usage
+
+input_pdfs = [r'C:\Users\Roma Invicta\source\repos\BassBoulder\PythonPySpark\Python Variant\PDFMERGE\PDFs\1.pdf', r'C:\Users\Roma Invicta\source\repos\BassBoulder\PythonPySpark\Python Variant\PDFMERGE\PDFs\2.pdf']
+
+output_pdf = r'C:\Users\Roma Invicta\source\repos\BassBoulder\PythonPySpark\Python Variant\PDFMERGE\PDFs\3.pdf'
+
+ 
+
+merge_pdfs(input_pdfs, output_pdf)
