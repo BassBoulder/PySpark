38c26bd7c6998da6b486e0434bc9ae1e8ea95f6d
Nick <90762430+BassBoulder@users.noreply.github.com>
2024-11-10 19:26:29 +0000

    Added testing for Leap Year & Completed Leap Year Exercism exercise

---
+++ Python Variant/TESTING/LeapYear.py
@@ -0,0 +1,13 @@
+def leap(year):
+    
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+    elif year % 4 == 0:
+        return True
+        
+    return False
+
+
+
+print(leap(2100))
