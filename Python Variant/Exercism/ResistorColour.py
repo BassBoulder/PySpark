19eb2522799a2ba61633d008f60690dd075ce013
Nick <90762430+BassBoulder@users.noreply.github.com>
2024-12-14 21:24:47 +0000

    Completed Resistor Colour Exercism exercise

---
+++ Python Variant/Exercism/ResistorColour.py
@@ -0,0 +1,7 @@
+colourDict = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4, "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+
+def color_code(color):
+    return colourDict.get(color.lower())
+
+def colors():
+    return list(colourDict.keys())
